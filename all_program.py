bc50dbf56faeaa8d8712a2c7c888c2470d38a54a
priyashukla1411 <priyashukla2k@gmail.com>
2022-02-21 18:25:19 +0530

    prog

---
+++ all_program.py
@@ -0,0 +1,27 @@
+import requests
+import json
+url = "https://www.fast2sms.com/dev/bulk"
+my_data = {
+
+	'sender_id': 'FSTSMS',
+	
+
+	'message': 'hello ',
+	
+	'language': 'english',
+	'route': 'p',
+
+	'numbers': '7889454758'	}
+headers = {
+	'authorization': 'moYvjT7tBylP5iWJkS8ecGs6LIN4hMb3wVCpnurqE2xADFUdX9uL4HzSshPCMoANlrf96R8YOFcjVnQe',
+
+}
+response = requests.request("POST",
+                            url,
+                            data = my_data,
+                            headers = headers)
+
+returned_msg = json.loads(response.text)
+  
+
+print(returned_msg['message'])
